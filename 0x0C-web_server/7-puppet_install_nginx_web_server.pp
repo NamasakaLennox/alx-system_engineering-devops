fba9ec15afe492d75813c3f6581015db981c84d3
NamasakaLennox <namasakalennox254@gmail.com>
2023-08-29 17:06:37 +0300

    setup config using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,7 @@
+# installs and configure nginx using puppet
+
+# Install Nginx web server (w/ Puppet)
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=dQw4w9WgXcQ permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
