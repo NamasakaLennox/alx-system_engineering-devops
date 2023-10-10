524fcd69d79bd9783388c1b02b8fadcf01a8c6de
NamasakaLennox <namasakalennox254@gmail.com>
2023-10-10 21:48:44 +0300

    returns the first 10 hot posts in a subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+module that gets the top 10 hot posts in a subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    gets the top ten hot posts from the subreddit passed as an argument
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'api-request by Namasaka'}
+    res = requests.get(url, headers=headers, allow_redirects=False)
+
+    if res.status_code == 200:
+        res = res.json()
+        posts = res['data']['children']
+        # print the first 10 items
+        for i in range(10):
+            print(posts[i]['data']['title'])
+    else:
+        print(None)
