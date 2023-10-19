1a506193116767a1db968d757ea2e79d960466d3
NamasakaLennox <namasakalennox254@gmail.com>
2023-10-19 19:33:48 +0300

    edited command path for the execute function

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,5 @@
 # changed the soft and hard ulimit limitation
 
 exec { 'comment out limit':
-  command => 'sed -i "s/holberton/#holberton/" /etc/security/limits.conf',
-  path    => '/usr/bin/env'
+  command => '/bin/sed -i "s/holberton/#holberton/" /etc/security/limits.conf',
 }
